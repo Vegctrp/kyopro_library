5ffc025b09c56bec708e74889b37c8dc6cec01b0
Vegctrp <kem.altair@gmail.com>
2020-02-01 15:52:34 +0900

    fix WeightedUnionFindTree

--- DataSets_Queries/WeightedUnionFindTree.cpp
+++ DataSets_Queries/WeightedUnionFindTree.cpp
@@ -1,23 +1,24 @@
 #include<stdio.h>
 #include<vector>
+#include<functional>
 
 using namespace std;
 typedef long long ll;
 
 template <typename Abel=ll> class WeightedUnionFind {
     public:
+    using Abel_func = function<Abel(Abel, Abel)>;
     std::vector<ll> parent;
     std::vector<ll> sizes;
     std::vector<Abel> diff_weight;
-    Abel Abel_unit = 0;                 //可換群の単位元 1
-    Abel Abel_plus(Abel a, Abel b){      //可換群の加法 ab
-        return a+b;
-    }
-    Abel Abel_minus(Abel a, Abel b){    //ab^-1
-        return a-b;
-    }
+    Abel Abel_unit;
+    Abel_func Abel_calc, Abel_calc_inv;
 
-    WeightedUnionFind(ll N) : parent(N), sizes(N, 1), diff_weight(N) {
+    WeightedUnionFind(ll N, 
+                    Abel Abel_unit = 0,
+                    Abel_func Abel_calc = [](ll a, ll b){return a+b;},
+                    Abel_func Abel_calc_inv = [](ll a, ll b){return a-b;}) 
+            : parent(N), sizes(N, 1), diff_weight(N), Abel_unit(Abel_unit), Abel_calc(Abel_calc), Abel_calc_inv(Abel_calc_inv) {
         for(ll i=0; i<N; i++){
             parent[i] = i;
             diff_weight[i] = Abel_unit;
@@ -26,18 +27,18 @@ template <typename Abel=ll> class WeightedUnionFind {
     ll find(ll x){     //経路圧縮しながらルート探索
         if(x==parent[x]) return x;
         ll r = find(parent[x]);
-        diff_weight[x] = Abel_plus(diff_weight[x], diff_weight[parent[x]]);
+        diff_weight[x] = Abel_calc(diff_weight[x], diff_weight[parent[x]]);
         return parent[x]=r;
     }
     bool unite(ll x, ll y, Abel w){ // yがxよりw大きいように結合
-        w = Abel_plus(w, weight(x));
-        w = Abel_minus(w, weight(y));
+        w = Abel_calc(w, weight(x));
+        w = Abel_calc_inv(w, weight(y));
         ll x_class = find(x);
         ll y_class = find(y);
         if(x_class == y_class) return false;
         if(sizes[x_class] < sizes[y_class]){
             std::swap(x_class, y_class);
-            w = Abel_minus(Abel_unit, w);
+            w = Abel_calc_inv(Abel_unit, w);
         }
         parent[y_class] = x_class;
         sizes[x_class] += sizes[y_class];
@@ -56,6 +57,6 @@ template <typename Abel=ll> class WeightedUnionFind {
         return diff_weight[x];
     }
     Abel diff(ll x, ll y){  // 二点の距離(x,yが違うクラスの場合は未定義)
-        return Abel_minus(weight(y), weight(x));
+        return Abel_calc_inv(weight(y), weight(x));
     }
 };
